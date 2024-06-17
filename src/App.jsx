91066150984a8435028a99daec1461a3ddd8455b
denisehoo <denisehooanita@gmail.com>
2024-06-18 00:01:46 +0800

    add div above canvas

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,17 @@ import { Experience } from './components/Experience'
 function App() {
   
   return (
-    <Canvas
-      camera={{
-        fov: 64,
-        position: [2.3, 1.5, 2.3],
-      }}
-      touch-action="auto!important">
-      <Experience />
-    </Canvas>
+    <div class="relative h-screen">
+      <div class="fixed top-0 left-0 w-screen h-screen z-40"></div>
+      <Canvas
+        camera={{
+          fov: 64,
+          position: [2.3, 1.5, 2.3],
+        }}
+        touch-action="auto">
+        <Experience />
+      </Canvas>
+    </div>
   )
 }
 
