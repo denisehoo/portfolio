00c3ed9fb6fcd2d9a1a593c273205be772bcaf7e
denisehoo <denisehooanita@gmail.com>
2024-06-18 00:04:39 +0800

    remove div above canvas

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,14 @@ import { Experience } from './components/Experience'
 function App() {
   
   return (
-    <div class="relative h-screen">
-      <div class="fixed top-0 left-0 w-screen h-screen z-40"></div>
-      <Canvas
-        camera={{
-          fov: 64,
-          position: [2.3, 1.5, 2.3],
-        }}
-        touch-action="auto">
-        <Experience />
-      </Canvas>
-    </div>
+    <Canvas
+      camera={{
+        fov: 64,
+        position: [2.3, 1.5, 2.3],
+      }}
+      touch-action="auto">
+      <Experience />
+    </Canvas>
   )
 }
 
